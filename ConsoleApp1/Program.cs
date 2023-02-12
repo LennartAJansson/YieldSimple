6b95fcfd021ba7c0abad87c504e21afee2f79e5d
Lennartajansson <lennart.a.jansson@outlook.com>
2023-02-12 20:29:41 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+foreach (string s in GetStrings())
+{
+    Console.WriteLine(s);
+}
+Console.WriteLine("Klart");
+
+static IEnumerable<string> GetStrings()
+{
+    foreach (string s in new string[] { "Ett", "Två", "Tre" })
+    {
+        yield return s;
+        Task.Delay(3000).Wait();
+    }
+}
